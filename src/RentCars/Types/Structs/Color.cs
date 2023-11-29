b92fd393773c4fa3d19563c409c469807ddd62e5
matheus proença <matheusproenca2020@hotmail.com>
2023-11-29 15:04:51 -0300

    Finalizando Req01

--- src/RentCars/Types/Structs/Color.cs
+++ src/RentCars/Types/Structs/Color.cs
@@ -1,6 +1,10 @@
+using System.Dynamic;
+
 namespace RentCars.Types;
 
 //1 - Crie um `Struct` para as **Cores**
 public struct Color
 {
+  public string Name { get; set; }
+  public string Hex { get; set; }
 }
